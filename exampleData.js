f1f1e29e241d7d119db41a3358f203808f1ffb02
Mateusz <mateusz.klemann@wp.pl>
2025-01-09 19:45:45 +0100

    Quick fixes

--- exampleData.js
+++ exampleData.js
@@ -6,12 +6,14 @@ db.drivers.drop();
 db.coordinators.drop();
 db.trucks.drop();
 db.tokens.drop();
+db.tasks.drop();
 
 print("Creating collections...");
 db.createCollection("drivers");
 db.createCollection("coordinators");
 db.createCollection("trucks");
 db.createCollection("tokens");
+db.createCollection("tasks");
 
 print("Inserting drivers...");
 db.drivers.insertMany([
@@ -21,7 +23,7 @@ db.drivers.insertMany([
     birthDate: "1985-06-15",
     email: "john.doe@example.com",
     phone: "123456789",
-    password: '$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6',
+    password: "$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6",
     fullAddress: {
       address: "123 Main St",
       city: "Springfield",
@@ -38,7 +40,7 @@ db.drivers.insertMany([
     birthDate: "1990-08-25",
     email: "jane.smith@example.com",
     phone: "987654321",
-    password: '$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6',
+    password: "$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6",
     fullAddress: {
       address: "456 Elm St",
       city: "Chicago",
@@ -55,7 +57,7 @@ db.drivers.insertMany([
     birthDate: "1987-11-02",
     email: "carlos.martinez@example.com",
     phone: "234567890",
-    password: '$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6',
+    password: "$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6",
     fullAddress: {
       address: "101 Pine St",
       city: "Los Angeles",
@@ -72,7 +74,7 @@ db.drivers.insertMany([
     birthDate: "1993-03-12",
     email: "emma.wilson@example.com",
     phone: "712345678",
-    password: '$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6',
+    password: "$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6",
     fullAddress: {
       address: "202 Birch Rd",
       city: "San Francisco",
@@ -93,7 +95,7 @@ db.coordinators.insertMany([
     birthDate: "1982-04-10",
     email: "alice.johnson@example.com",
     phone: "122334455",
-    password: '$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6',
+    password: "$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6",
     fullAddress: {
       address: "789 Maple Ave",
       city: "Houston",
@@ -109,7 +111,7 @@ db.coordinators.insertMany([
     birthDate: "1975-12-05",
     email: "bob.brown@example.com",
     phone: "566778899",
-    password: '$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6',
+    password: "$2b$10$HTQHn38WEZIGtsaga07rqe6LTbarGWVJd8w/3xKv2CR4aTyqpZYP6",
     fullAddress: {
       address: "321 Oak St",
       city: "Dallas",
@@ -162,6 +164,111 @@ db.trucks.insertMany([
     maxFuel: 400,
     status: "operational",
     currentDriver: null,
+  },
+]);
+
+print("Fetching truck licensePlates for task assignment...");
+const johnTruck = db.trucks.findOne({ currentDriver: johnId }).licensePlate;
+const emmaTruck = db.trucks.findOne({ currentDriver: emmaId }).licensePlate;
+
+print("Inserting tasks...");
+db.tasks.insertMany([
+  {
+    name: "Fruits to FruitCompany",
+    status: "Waiting",
+    driver: "",
+    truck: "",
+    deliveryDate: "2025-02-01",
+    deliveryAddress: {
+      address: "429 Book St",
+      city: "Dallas",
+      state: "TX",
+      postalCode: "75201",
+      country: "USA",
+    },
+    description: "A delivery of bananas.",
+  },
+  {
+    name: "Beans to BeanCompany",
+    status: "In progress",
+    driver: {
+      name: "John",
+      surname: "Doe",
+      email: "john.doe@example.com",
+      phone: "123456789",
+    },
+    truck: johnTruck,
+    deliveryDate: "2025-02-04",
+    deliveryAddress: {
+      address: "233 Bean St",
+      city: "Dallas",
+      state: "TX",
+      postalCode: "75201",
+      country: "USA",
+    },
+    description: "A delivery of beans.",
+  },
+  {
+    name: "Butter to ButterCompany",
+    status: "In progress",
+    driver: {
+      name: "Emma",
+      surname: "Wilson",
+      birthDate: "1993-03-12",
+      email: "emma.wilson@example.com",
+      phone: "712345678",
+    },
+    truck: emmaTruck,
+    deliveryDate: "2025-02-01",
+    deliveryAddress: {
+      address: "123 Butter St",
+      city: "Dallas",
+      state: "TX",
+      postalCode: "75201",
+      country: "USA",
+    },
+    description: "A delivery of butter.",
+  },
+  {
+    name: "Computers to ComputerCompany",
+    status: "Completed",
+    driver: {
+      name: "John",
+      surname: "Doe",
+      email: "john.doe@example.com",
+      phone: "123456789",
+    },
+    truck: johnTruck,
+    deliveryDate: "2025-02-01",
+    deliveryAddress: {
+      address: "398 Int St",
+      city: "Dallas",
+      state: "TX",
+      postalCode: "75201",
+      country: "USA",
+    },
+    description: "A delivery of computers.",
+  },
+  {
+    name: "Water to WaterCompany",
+    status: "On hold",
+    driver: {
+      name: "John",
+      surname: "Doe",
+      email: "john.doe@example.com",
+      phone: "123456789",
+    },
+    truck: johnTruck,
+    deliveryDate: "2025-02-02",
+    deliveryAddress: {
+      address: "338 Saint St",
+      city: "Dallas",
+      state: "TX",
+      postalCode: "75201",
+      country: "USA",
+    },
+    description:
+      "A delivery of water. On hold beacuse of missing initial payment.",
   },
 ]);
 
